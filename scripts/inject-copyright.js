8a9fc9705c95e8861c1e5fcbf10c32b3763f5013
chakiri <nijine.chakiri@gmail.com>
2023-07-09 16:45:31 +0800

    Update inject-copyright.js

--- scripts/inject-copyright.js
+++ scripts/inject-copyright.js
@@ -54,14 +54,14 @@ hexo.extend.filter.register('theme_inject', function(injects) {
             </div>
           </div>
         <% } %>
-        <% if (page.reproduce) { %>
-          <div class="license-meta-item">
-            <div>未经授权不得转载</div>
-            <div><%= page.reproduce ? '是' : '否' %></div>
-          </div>
-        <% } %>
       </div>
       <div class="license-icon iconfont"></div>
+      <% if (page.reproduce) { %>
+        <div class="license-meta-item">
+          <div>未经授权不得转载</div>
+          <div><%= page.reproduce ? '是' : '否' %></div>
+        </div>
+      <% } %>
     </div>
   <% } %>
   
