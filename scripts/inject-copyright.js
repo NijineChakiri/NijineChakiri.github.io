f82c95a0f467b17a3f1f9dda590c0337107f357f
chakiri <nijine.chakiri@gmail.com>
2023-07-09 15:53:13 +0800

    update inject

--- scripts/inject-copyright.js
+++ scripts/inject-copyright.js
@@ -1,5 +1,5 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
-  injects.postCopyright.raw('default', `
+  injects.postCopyright.raw('reproduce', `
     <div class="additional-copyright">
       <p>未经授权不得转载</p>
     </div>
