8de6212c86a87ebe26e50579b76b78e248083b7c
chakiri <nijine.chakiri@gmail.com>
2023-07-09 22:31:15 +0800

    inject hexo-renderer-marked

--- scripts/custom-marked.js
+++ scripts/custom-marked.js
@@ -4,8 +4,8 @@ hexo.extend.filter.register('marked:renderer', function(renderer) {
   // 扩展 text 方法
   renderer.text = function(text) {
     // 自定义渲染逻辑
-    if (text.startsWith('&[') && text.endsWith(']&')) {
-      const string = text.slice(2, -2);
+    if (text.startsWith('@') && text.endsWith('@')) {
+      const string = text.slice(1, -1);
       return `<span class="Opus Chords Sans Std">${string}</span>`;
     }
 
